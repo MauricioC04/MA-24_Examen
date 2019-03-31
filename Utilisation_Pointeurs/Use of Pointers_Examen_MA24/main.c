2210568676581b7b736b833b36a40fc3d695ee42
Mauricio <mauricio.costa-cabral@cpnv.ch>
2019-03-31 13:04:27 +0200

    Use of Pointers - First manually test - User value (successful)

--- Utilisation_Pointeurs/Use of Pointers_Examen_MA24/main.c
+++ Utilisation_Pointeurs/Use of Pointers_Examen_MA24/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Declaration de fonction
+void Decomposition_Annee_Mois_Jour(int* anneePointeur, int* moisPointeur, int* jourPointeur);
+
+
 /** \brief Fonction principal Main du programme
  *
  * \return int
@@ -11,21 +15,23 @@ int main()
     /*Initialisation des variables à zéro*/
     int annee = 0;
     int mois = 0;
-
-    /*Premier test automatique avec une valeur prédéfinie. Resultat attendu: 5 annees, 3 mois, 6 jours*/
-    int jour = 1896;
+    int jour = 0;
 
 
-    printf("DECOMPOSITION DU NOMBRE DE JOURS EN ANNEE, MOIS ET JOURS\n\n");
+    printf("\n    DECOMPOSITION DU NOMBRE DE JOURS EN ANNEE, MOIS ET JOURS\n\n");
 
-    printf("Voici la decomposition pour %i jours:\n\n", jour);
+    printf("    Veuillez entrer un nombre de jours: "); //Test avec une valeur introduit par l'utilisateur. Exemple: 6772 jours. Resultat attendu: 18 annees, 9 mois et 22 jours.
+    scanf("%i", &jour);
+
+    printf("\n\n    Voici la decomposition pour %i jours:\n\n", jour);
 
     //Utilisation de la fonction de décomposition. Récupération des adresses mémoire des variables annee, mois, jour
     Decomposition_Annee_Mois_Jour(&annee, &mois, &jour);
 
-    printf("Annees: %i\n", annee);
-    printf("Mois: %i\n", mois);
-    printf("Jours: %i\n", jour);
+    //Affichage de la decomposition
+    printf("    Annees:      %i\n", annee);
+    printf("    Mois:        %i\n", mois);
+    printf("    Jours:       %i\n\n", jour);
 
     return EXIT_SUCCESS;
 }
